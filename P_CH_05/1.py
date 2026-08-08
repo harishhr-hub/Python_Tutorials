bdc2872e31437db32aca6d2909396b8541b912e6
Harish H R <harishhr3457@gmail.com>
2026-08-08 23:33:26 +0530

    Add Python practice files

---
+++ P_CH_05/1.py
@@ -0,0 +1,9 @@
+#Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up! 
+langs = {
+  "Madad": "help",
+  "paani": "water",
+  "billi": "cat"
+}
+
+lang = input("enter the word to know the meaning:")
+print(langs[lang])
---
+++ P_CH_05/6.py
@@ -0,0 +1,17 @@
+#Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume that the names are unique. 
+d = {}
+name = input("enter the name:")
+lang = input("enter the language:")
+d.update({name: lang})
+name = input("enter the name:")
+lang = input("enter the language:")
+d.update({name: lang})
+name = input("enter the name:")
+lang = input("enter the language:")
+d.update({name: lang})
+name = input("enter the name:")
+lang = input("enter the language:")
+d.update({name: lang})
+print(d)
+
+
